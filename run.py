a62ce011eac0e7cb68da024dca346dae4fc7c7ee
Alexander Lee <cloudstrife79@gmail.com>
2021-09-22 12:19:41 -0400

    clarify timezone

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ def hello_world():
 
 @app.route('/time')
 def the_time_is_now():
-    return 'The time is {}'.format(datetime.strftime(datetime.now(), "%H:%M:%S"))
+    return 'The time (UTC) is {}'.format(datetime.strftime(datetime.utcnow(), "%H:%M:%S"))
 
 app.run(host='0.0.0.0',
         port=8080,
